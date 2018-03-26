a0c925bb005d9dc46221faa83a2ff733918c53a1
Ouandi Hoffer <hofferouandi@yahoo.fr>
2018-03-27 00:45:10 +0200

    New commit

--- server.js
+++ server.js
@@ -13,9 +13,59 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
+io.on('connection', (client) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  var userName;
+
+	console.log("user connected!");
+
+	client.emit('message', 'please insert user name');
+
+    
+
+	client.on('message', function(message){
+
+        if (!userName) {
+
+			userName = message;
+
+			console.log(userName + ' is connected :)');
+
+			client.emit('message', 'Welcome ' + userName);
+
+			client.broadcast.emit('message', userName + ' is connected');
+
+		}
+
+		else {
+
+			client.emit('message', 'me: ' + message);
+
+			client.broadcast.emit('message', userName + ' says: ' + message);
+
+		}
+
+    });
+
+
+
+    client.on('disconnect', function() {
+
+		if (userName) {
+
+			console.log(userName + " left");
+
+			client.broadcast.emit('message', userName + ' left us :(');
+
+		}
+
+		else {
+
+			console.log("anonymous left");
+
+		}
+
+    });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
